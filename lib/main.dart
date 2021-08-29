6535f611e1b8d5ded877b7f8cdcb83c34661d9ac
Jiaxen <jiaxen.lau2@gmail.com>
2021-08-29 11:56:58 +0100

    added questions and refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizbrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -37,7 +40,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizbrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +64,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                checkAnswer(true);
               },
             ),
           ),
@@ -79,19 +82,55 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(children: scoreKeeper)
       ],
     );
   }
-}
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+  List<Widget> scoreKeeper = [];
+
+  void checkAnswer(bool userPickedAnswer) {
+    if (quizbrain.quizFinished()) {
+      showEndDialog();
+    } else {
+      setState(() {
+        if (quizbrain.getQuestionAnswer() == userPickedAnswer) {
+          scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+        } else {
+          scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+        }
+        quizbrain.nextQuestion();
+      });
+      if (quizbrain.quizFinished()) {
+        showEndDialog();
+      }
+    }
+  }
+
+  void showEndDialog() {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Quiz over!'),
+        content: const Text('You finished the quiz'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              setState(() {
+                quizbrain.restartQuestions();
+                scoreKeeper = [];
+              });
+              Navigator.pop(context, 'Restart');
+            },
+            child: const Text('Restart'),
+          ),
+        ],
+      ),
+    );
+  }
+}
